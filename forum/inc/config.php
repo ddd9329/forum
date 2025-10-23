105abeced76573b83e3c12d3ff4abbad5d82ad74
ddd9329 <149134337+ddd9329@users.noreply.github.com>
2025-10-23 15:07:37 +0200

    Delete forum directory

--- forum/inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'forum');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
-define('BASE_URL', 'http://localhost/projects/forum/public');
--- forum/public/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../inc/auth.php';
-logout();
-header('Location: ' . base_url('index.php'));
-exit;
